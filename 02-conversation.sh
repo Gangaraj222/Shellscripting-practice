8d9b1eecfd691d2bcaca0f9f3efda96e44385605
Gangaraju <ug.raju11@gmail.com>
2025-05-21 14:51:26 +0530

    files added

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "trump: Hey Musk, How are you?"
+echo "Musk: Hello Trump, I am fine. how are you doing"
+echo "trump: I am fine too. what's up?"
+echo ":Musk: Nothing, just going to mars now, will you come?"
+echo "Trump: sorry, you acrry on! I will come once you come back"
